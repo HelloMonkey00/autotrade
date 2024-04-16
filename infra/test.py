04c0360425a282131b02614c36f471b3e6a975fc
JasonFei2023 <kevinhome2007@gmail.com>
2024-04-17 02:01:52 +0800

    init Dockerfile

--- infra/test.py
+++ infra/test.py
@@ -1,7 +1,7 @@
 from moomoo import *
 
 quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)  # 创建行情对象
-print(quote_ctx.get_market_snapshot('HK.00700'))  # 获取港股 HK.00700 的快照数据
+(quote_ctx.get_market_snapshot('HK.00700'))  # 获取港股 HK.00700 的快照数据
 quote_ctx.close() # 关闭对象，防止连接条数用尽
 
 
